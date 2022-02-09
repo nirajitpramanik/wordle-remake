d213880ee55fafdb488fa412cc8cb9e207d04c21
LeoJit <leojit2213@gmail.com>
2022-02-09 19:40:59 +0530

    Added code to read key

--- wordle/settings.py
+++ wordle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,9 @@ PROJECT_DIR = os.path.join(BASE_DIR, "wordle")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+with open("wordle/secret_key.json", "r") as fp:
+    key = json.load(fp)
+SECRET_KEY = key[0]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
